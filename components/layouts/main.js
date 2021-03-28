8eb6e7befdd048a86927384d01d633071a5f2053
myradov <myradov-be@outlook.com>
2021-03-29 03:20:50 +0500

    Admin and About page

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -26,7 +26,7 @@ export default function MainLayout({ children }) {
                 />
                 <meta name="og:title" content={siteTitle} />
                 <meta name="twitter:card" content="summary_large_image" />
-                <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+                <script src="https://identity.netlify.com/v1/netlify-identity.js"></script>
                 
                 <title>{siteTitle}</title>  
             </Head>
--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,28 @@
-
+import Image from 'next/image'
+import styles from '../styles/About.module.css'
 
 const About = () => {
     return (
         <>
-          About page 
+          <section className={styles.about}>
+            <div className={styles.title}>
+              <h2>About us</h2>
+            </div>
+            <article className={styles.article}>
+              <div>
+                <h2>Turkmen Carpets</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui odio in nobis nulla doloremque praesentium quis id excepturi eaque magnam.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui odio in nobis nulla doloremque praesentium quis id excepturi eaque magnam.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui odio in nobis nulla doloremque praesentium quis id excepturi eaque magnam.</p>
+              </div>
+              <Image
+                src="/about.jpg"
+                width={600}
+                height={660}
+                quality={30}
+              />
+            </article>
+          </section> 
         </>
     )
 }
