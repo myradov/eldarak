c4b277168a6d2ae03918ffad4086a45f2253e265
myradov <myradov-be@outlook.com>
2021-03-29 03:55:56 +0500

    About page

--- pages/about.js
+++ pages/about.js
@@ -11,10 +11,11 @@ const About = () => {
             <article className={styles.article}>
               <div>
                 <h2>Turkmen Carpets</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui odio in nobis nulla doloremque praesentium quis id excepturi eaque magnam.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui odio in nobis nulla doloremque praesentium quis id excepturi eaque magnam.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eum minus esse dicta praesentium. Doloribus omnis, nulla rerum numquam tenetur ad accusantium illo natus laboriosam quaerat? Libero veniam nulla velit eveniet possimus, amet in earum, aperiam aliquid labore necessitatibus perspiciatis?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eum minus esse dicta praesentium. Doloribus omnis, nulla rerum numquam tenetur ad accusantium illo natus laboriosam quaerat? Libero veniam nulla velit eveniet possimus, amet in earum, aperiam aliquid labore necessitatibus perspiciatis?</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui odio in nobis nulla doloremque praesentium quis id excepturi eaque magnam.</p>
               </div>
+              <div className={styles.bgShadow}></div>
               <Image
                 src="/about.jpg"
                 width={600}
